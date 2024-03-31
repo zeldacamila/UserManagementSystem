b49f3407f8621805e47698ffc2d0eb9d42c1756b
Maria C Recuero <zeldacamila0913@gmail.com>
2024-03-31 17:18:39 -0500

    add create user endpoint

--- loginRegisterAPI/userApi/serializers.py
+++ loginRegisterAPI/userApi/serializers.py
@@ -6,3 +6,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        return {
+            "username": user.username,
+            "email": user.email,
+        }
--- loginRegisterAPI/userApi/views.py
+++ loginRegisterAPI/userApi/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 
 
-def index(request):
-    return HttpResponse("Hello world!")
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
